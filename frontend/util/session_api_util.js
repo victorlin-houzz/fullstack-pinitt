57fb2bc44069cca18a8bd194d0677dafbcbc2034
Victor Lin <swissashley@hotmail.com>
2016-09-05 17:59:38 -0700

    Fixed user signup api

--- frontend/util/session_api_util.js
+++ frontend/util/session_api_util.js
@@ -11,7 +11,7 @@ export const login = function(user, success, error) {
 export const signup = function(user, success, error) {
 	$.ajax({
 		method: 'POST',
-		url: '/api/user',
+		url: '/api/users',
 		data: user,
 		success,
 		error
