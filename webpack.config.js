08069ce64e1d513fdcb5020b4b2c56d1568ea957
Victor Lin <swissashley@hotmail.com>
2016-09-07 18:11:51 -0700

    modify webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+var webpack = require('webpack');
+
 module.exports = {
   context: __dirname,
   entry: './frontend/pinitt.jsx',
@@ -6,6 +8,18 @@ module.exports = {
     path: path.join(__dirname, 'app', 'assets', 'javascripts'),
     filename: 'bundle.js'
   },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: false
+      }
+    })
+  ],
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
